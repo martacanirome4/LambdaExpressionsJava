259f534922bd2be6dde74f04dabdb72ae0483afe
MartaCanino <martacaninoromero@gmail.com>
2023-03-16 08:55:05 +0100

    makefile y manifest

--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -1,7 +1,14 @@
 package aplicacion;
+import mates.*;
+from mates import Mates;
 
 public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
+        Mates mates = new Mates();
+
+        List<Integer> numeros = List.of(1, 2, 3, 4, 5);
+        List<Integer> squares = cuadradosLista(numbers);
+        System.out.println(squares);
     }
 }
--- src/mates/Mates.java
+++ src/mates/Mates.java
@@ -1,2 +1,11 @@
 package mates;
+import mates.*;
+import java.util.ArrayList;
 
+public class Mates {
+    public static List<Integer> cuadradosLista(List<Integer> numeros) {
+        List<Integer> cuadrados = new ArrayList<>();
+        numeros.forEach(numero -> cuadrados.add(numero * numero));
+        return cuadrados;
+    }
+}
