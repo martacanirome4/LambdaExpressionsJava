a8944d8ddaf3d3b3e0082992d23507abf46e4635
MartaCanino <martacaninoromero@gmail.com>
2023-03-17 10:48:19 +0100

    Corregir llamadas desde main

--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -8,49 +8,57 @@ public class Main {
         // TODO code application logic here
         Mates mates = new Mates();
 
-        // 1
         List<Integer> numeros = List.of(1, 2, 3, 4, 5);
         System.out.println("Números: "+numeros);
 
-        // 2
+        // 1
         List<Integer> cuadrados = mates.cuadradosLista(numeros);
         System.out.println("Cuadrados: "+cuadrados);
 
-        // 3
+        // 2
         int suma = mates.sumaLista(numeros);
         System.out.println("Suma: "+suma);
 
-        // 4
+        // 3
         List<Integer> pares = mates.paresLista(numeros);
         System.out.println("Pares: "+pares);
 
-        // 5
+        // 4
         int sumaPares = mates.sumaPares(numeros);
         System.out.println("Suma de pares: "+sumaPares);
 
-        // 6
+        // 5
         double media = mates.mediaLista(numeros);
         System.out.println("Media: "+media);
 
-        // 7
+        // 6
         double desviacion = mates.desviacionTipica(numeros);
         System.out.println("Desviación típica: "+desviacion);
 
-        // 8 + 10
+        // 7
         int menor = mates.menorLista(numeros);
         System.out.println("Menor elemento de la lista: "+menor);
 
-        // 9
         List<String> mayusculas = List.of("HOLA", "QUE", "TAL");
         System.out.println("Mayúsculas: "+mayusculas);
 
-        // 11
+        // 8 y 10
         List<String> minusculas = mates.aMinusculas(mayusculas);
         System.out.println("Minúsculas: "+minusculas);
 
-        //
+        // 9
         String concatenacion = mates.concatenarCadenas(minusculas);
         System.out.println("Concatenación: "+concatenacion);
+
+        List<String> palabras = List.of("AVIÓN", "JUGUETES", "PALABRAS", "AFRICA");
+        System.out.println("Palabras: "+palabras);
+        // 11
+        List<String> palabrasSinA = mates.noEmpiezaPorA(palabras);
+        System.out.println("Palabras que no empiezan por 'a': "+palabrasSinA);
+
+        // 12
+        List<Integer> mayorQueSeis = mates.por2MayorQue6(numeros);
+        System.out.println("Por 2 y mayor que 6: "+mayorQueSeis);
 
 
     }
