801f0c5e451b1723704ca97e8ed96a2af7e37489
MartaCanino <martacaninoromero@gmail.com>
2023-03-17 10:11:46 +0100

    Ejercicios terminados

--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -1,14 +1,59 @@
 package aplicacion;
 import mates.*;
-from mates import Mates;
+import mates.Mates;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         Mates mates = new Mates();
 
+        // 1
         List<Integer> numeros = List.of(1, 2, 3, 4, 5);
-        List<Integer> squares = cuadradosLista(numbers);
-        System.out.println(squares);
+        System.out.println("Números: "+numeros);
+
+        // 2
+        List<Integer> cuadrados = mates.cuadradosLista(numeros);
+        System.out.println("Cuadrados: "+cuadrados);
+
+        // 3
+        int suma = mates.sumaLista(numeros);
+        System.out.println("Suma: "+suma);
+
+        // 4
+        List<Integer> pares = mates.paresLista(numeros);
+        System.out.println("Pares: "+pares);
+
+        // 5
+        int sumaPares = mates.sumaPares(numeros);
+        System.out.println("Suma de pares: "+sumaPares);
+
+        // 6
+        double media = mates.mediaLista(numeros);
+        System.out.println("Media: "+media);
+
+        // 7
+        double desviacion = mates.desviacionTipica(numeros);
+        System.out.println("Desviación típica: "+desviacion);
+
+        // 8 + 10
+        int menor = mates.menorLista(numeros);
+        System.out.println("Menor elemento de la lista: "+menor);
+
+        // 9
+        List<String> mayusculas = List.of("HOLA", "QUE", "TAL");
+        System.out.println("Mayúsculas: "+mayusculas);
+
+        // 11
+        List<String> minusculas = mates.aMinusculas(mayusculas);
+        System.out.println("Minúsculas: "+minusculas);
+
+        //
+        String concatenacion = mates.concatenarCadenas(minusculas);
+        System.out.println("Concatenación: "+concatenacion);
+
+
     }
+
+
 }
--- src/mates/Mates.java
+++ src/mates/Mates.java
@@ -1,11 +1,114 @@
 package mates;
 import mates.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Mates {
+
+    // ejercicio 1
     public static List<Integer> cuadradosLista(List<Integer> numeros) {
         List<Integer> cuadrados = new ArrayList<>();
         numeros.forEach(numero -> cuadrados.add(numero * numero));
         return cuadrados;
     }
+
+    // ejercicio 2
+    public static int sumaLista(List<Integer> numeros) {
+        int suma = numeros
+                    .stream()
+                    .reduce(0, (a, b) -> a + b);
+                    // 0: valor inicial de la operación y resultado predeterminado si la secuencia está vacía
+                    // a = subtotal, b = elemento de la lista
+        return suma;
+    }
+
+    // ejercicio 3
+    public static int sumaPares(List<Integer> numeros) {
+        int suma = numeros
+                    .stream()
+                    .filter(numero -> numero % 2 == 0)
+                    .reduce(0, (a, b) -> a + b);
+        return suma;
+    }
+
+    // ejercicio 4
+    public static List<Integer> paresLista(List<Integer> numeros) {
+        List<Integer> pares = new ArrayList<>();
+        numeros.forEach(numero -> {
+            if (numero % 2 == 0) {
+                pares.add(numero);
+            }
+        });
+        return pares;
+    }
+
+
+    // ejercicio 5
+    public static double mediaLista(List<Integer> numeros) {
+        int suma = numeros
+                    .stream()
+                    .reduce(0, (a, b) -> a + b);
+        int media = suma / numeros.size();
+        return media;
+    }
+
+
+    // ejercicio 6
+    public static double desviacionTipica(List<Integer> numeros) {
+        int suma = numeros
+                .stream()
+                .reduce(0, (a, b) -> a + b);
+        int media = suma / numeros.size();
+        double desviacion = Math.sqrt(suma / numeros.size());
+        return desviacion;
+    }
+
+    // ejercicio 7
+    public static int menorLista(List<Integer> numeros) {
+        return numeros.stream().min(Integer::compareTo).orElse(null);
+    }
+
+
+    // ejercicio 8 / 10
+    public static List<String> aMinusculas(List<String> lista) {
+        List<String> minusculas = new ArrayList<>();
+        lista.forEach(palabra -> minusculas.add(palabra.toLowerCase()));
+        return minusculas;
+    }
+
+
+    // ejercicio 9
+    public static String concatenarCadenas(List<String> al) {
+        String cadena = al.stream().reduce("", (a, b) -> a + b);
+        return cadena;
+    }
+
+
+    // ejercicio 11
+    // Devuelve una lista con todos los elementos de otra lista de String que no empiezen por la letra ‘A’
+    public static List<String> noEmpiezaPorA(List<String> al) {
+        List<String> sinA = new ArrayList<>();
+        al.forEach(palabra -> {
+            if (!palabra.startsWith("A")) {
+                sinA.add(palabra);
+            }
+        });
+        return sinA;
+    }
+
+    // ejercicio 12
+    // Devuelve una lista con todos los números mayores que seis resultantes de multiplicar por 2 todos los elementos
+    // de una lista de entrada
+    public static List<Integer> por2MayorQue6(List<Integer> lista) {
+        List<Integer> multiplicados = new ArrayList<>();
+        lista.forEach(numero -> {
+            if (numero * 2 > 6) {
+                multiplicados.add(numero * 2);
+            }
+        });
+        return multiplicados;
+    }
+
+
+
 }
